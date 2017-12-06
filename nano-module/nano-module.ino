b0c89fa3b0eed5625af0ace9ead7670d855cf044
sj96 <sj.dev.96@gmail.com>
2017-12-06 08:17:52 +0700

    update 8:17 6/12/2017
    update nano module
    change nodeMCU request

--- nano-module/nano-module.ino
+++ nano-module/nano-module.ino
@@ -1,12 +1,12 @@
 /*
    RFID module
-    TX - pin 6
-    RX - pin 7
+    TX - pin 3
+    RX - pin 4
    SD card attached to SPI bus as follows:
  ** MOSI - pin 17
  ** MISO - pin 18
  ** SCK - pin 19
- ** CS - pin 4 (for MKRZero SD: SDCARD_SS_PIN)
+ ** CS - pin 2 (for MKRZero SD: SDCARD_SS_PIN)
 */
 
 /*
@@ -22,33 +22,37 @@
    var
 */
 SerialCommand sCmd;
-rdm630 rfid(6, 7);  //TX-pin of RDM630 connected to Arduino pin 6
+rdm630 rfid(3, 4);  //TX-pin of RDM630 connected to Arduino pin D3
 
 File myFile;
 String eventName = "";
 String logName = "" ;
-unsigned long  rfidCard;
+unsigned long  rfidCard, oldCard;
+
+unsigned long delaytime = 0;
+
 
 void setup() {
   Serial.begin(115200);
   rfid.begin();
 
   // Một số hàm trong thư viện Serial Command
-  sCmd.addCommand("SetEvent",   setEventName);
-  sCmd.addCommand("SetTimeLog",   setTimeLog);
-  
+  sCmd.addCommand("SetEvent", setEventName);
+  sCmd.addCommand("SetTime", setTimeLog);
+
   //đợi cài đặt sự kiện và thời gian ghi log
   while (eventName == "" || logName == "") {
     sCmd.readSerial();
   }
-  writeLog("Time: "+logName);
-  writeLog("event: "+eventName);
-  
   //ktra module SD card
-  if (!SD.begin(4)) {
+  if (!SD.begin(2)) {
     Serial.println("Error 1");
     writeLog("Error: SD card module not connect.");
   }
+  Serial.println("readly");
+  writeLog("Time: " + logName);
+  writeLog("event: " + eventName);
+
 }
 
 void loop() {
@@ -58,10 +62,22 @@ void loop() {
   //kiểm tra tính hiệu từ bộ đọc
   rfidCard = readRFID();
   if ( rfidCard != 0) {
-    writeLog("Read RFID card");
-    writeLogRFID(rfidCard+"");
-    Serial.print("Attendance ");
-    Serial.println(rfidCard);
+    if (delaytime != 0 && oldCard == rfidCard) {
+      delaytime = millis();
+    }
+    else {
+      delaytime = millis();
+      oldCard = rfidCard;
+      //      writeLog("Read RFID card");
+      writeLogRFID(String(rfidCard));
+      Serial.println("Attendance " + String(rfidCard));
+    }
+  }
+  else {
+    if (millis() - delaytime > 1000 && delaytime > 0) {
+      Serial.println("ok! next Card.");
+      delaytime = 0;
+    }
   }
 }
 
@@ -83,16 +99,21 @@ unsigned long int readRFID() {
 
 void writeLog(String log) {
   File logFile;
-  logFile = SD.open("log/" + logName+".txt", FILE_WRITE);
+  logFile = SD.open("log/" + logName + ".log", FILE_WRITE);
   logFile.println(log);
   logFile.close();
 }
 void writeLogRFID(String log) {
   File logFile;
-  logFile = SD.open("attendance/" + eventName+".txt", FILE_WRITE);
+  logFile = SD.open(eventName + ".log", FILE_WRITE);
+  if (!logFile) {
+    Serial.println("not open file");
+  }
   logFile.println(log);
   logFile.close();
+  Serial.println("log Rfid");
 }
+
 //set event name: gán tên cho sự kiện để tạo file log điểm danh cho từng sự kiện riêng biệt
 void setEventName() {
   char *arg;
--- nodeMCU/nodeMCU.ino
+++ nodeMCU/nodeMCU.ino
@@ -1,4 +1,5 @@
 #include <ArduinoJson.h>
+#include <SerialCommand.h>
 
 #include <Arduino.h>
 
@@ -14,19 +15,29 @@ LiquidCrystal lcd(D5, D0, D4, D3, D2, D1);
 const char* ssid = "68C/10a";
 const char* password =  "Khuvuc6@2017";
 
-const char* host = "192.168.1.7:80";
+const String host = "192.168.1.7:80";
 
-const char* privateKey = "";
+const char* privateKey = "0388a47143e4ddca4c0c6c1be40e07ef63ea2091";
 
-int count;
+int delayTime = -1;
+
+String event = "";
+String date = "";
+
+SerialCommand sCmd;
 
 void setup() {
   //cài đặt LCD
   lcd.begin(16, 2);
+
   //mở giao tiếp
   Serial.begin(115200);
+
+  sCmd.addCommand("Attendance ", attendance);
+
   //khởi tạo kết nối
   WiFi.begin(ssid, password);
+
   //kiểm tra kết nối host
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
@@ -34,63 +45,72 @@ void setup() {
     printLCD("Connecting..", 0);
   }
   Serial.println("Connected");
-  printLCD("Connected", 0, 0);
-  count = 0;
+  printLCD("Connected", 0);
+  printLCD("Get Event - Time", 1);
+  //lấy mã sự kiện
+  while (event == "" || event == "Error" || event == "no Event") {
+    event = getEvent();
+    if (event == "no Event") printLCD(event, 1);
+  }
+  Serial.println("SetEvent " + String(event));
+
+  //lấy ngày tháng
+  while (date == "" || date == "Error") {
+    date = getDate();
+  }
+
+  Serial.print("SetTime " + String(date));
 }
 
 
 void loop() {
-  
+  if (delayTime < 0) {
+    String str = event + "-" + date;
+    printLCD(str, 0);
+  }
+  else {
+    delayTime--;
+  }
+  //đợi lệnh
+  sCmd.readSerial();
 }
 
-boolean sendData(String ID) {
+boolean attendance(String rfid) {
   if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
-
+    HTTPRequest();
     HTTPClient http;
+    JsonBuffer<200> jsonBuffer;
+    String post = String("key=") + privateKey + String("rfid=") + rfid;
 
-    http.begin("http://" + host + "/post.php?ID=1&key=1"); //Specify the URL and certificate
-    int httpCode = http.GET();                                                  //Make the request
+    http.begin("http://" + host + "/attendance"); //Specify the URL and certificate
+    int httpCode = http.POST(post);                                                //Make the request
 
     if (httpCode > 0) { //Check for the returning code
-      Serial.print(count + " - ");
-      count++;
       String payload = http.getString();
-      //        Serial.println(httpCode);
-      Serial.println(payload);
-      printLCD(payload, 1);
+
+
     }
 
     else {
-      Serial.println("Error on HTTP request");
+      Serial.println("Error");
     }
 
     http.end(); //Free the resources
   }
 }
 
-String getTime() {
-  String result = "";
-  if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
-    HTTPClient http;
-    http.begin("http://" + host + "/getTime.php"); //Specify the URL and certificate
-    int httpCode = http.GET();                                                  //Make the request
-
-    if (httpCode > 0) { //Check for the returning code
-      result = http.getString();
-    }
-    else {
-      result = "Error";
-    }
-    http.end(); //Free the resources
-  }
-  return result;
+String getDate() {
+  String url = "http://" + host + "/getTime.php?type=date";
+  return Get(url);
 }
+
 String getEvent() {
   String result = "";
+  String post = String("key=") + privateKey;
   if ((WiFi.status() == WL_CONNECTED)) { //Check the current connection status
     HTTPClient http;
     http.begin("http://" + host + "/getEvent.php"); //Specify the URL and certificate
-    int httpCode = http.GET();                                                  //Make the request
+    int httpCode = http.POST(post);                                                  //Make the request
 
     if (httpCode > 0) { //Check for the returning code
       result = http.getString();
@@ -104,8 +124,12 @@ String getEvent() {
 }
 
 void printLCD(String str, int line) {
+  int x = 0;
   clearLCD(line);
-  lcd.setCursor(0, line);
+  if (str.length() <= 16) {
+    x = (16 - str.length()) / 2;
+  }
+  lcd.setCursor(x, line);
   // In ra dong chu
   lcd.print(str);
 }
@@ -113,5 +137,33 @@ void printLCD(String str, int line) {
 void clearLCD(int line) {
   lcd.setCursor(0, line);
   lcd.print("                ");
+}
+
+String Post(String host, String post){
+  String result = "Error";
+  if ((WiFi.status() == WL_CONNECTED)) { 
+    HTTPClient http;
+    http.begin(host); 
+    int httpCode = http.POST(post);                                                  //Make the request
+    if (httpCode > 0) { 
+      result = http.getString();
+    }
+    http.end();
+  }
+  return result;
+}
+
+String Get(String host){
+  String result = "Error";
+  if ((WiFi.status() == WL_CONNECTED)) { 
+    HTTPClient http;
+    http.begin(host); 
+    int httpCode = http.GET();                                                  
+    if (httpCode > 0) { 
+      result = http.getString();
+    }
+    http.end(); //Free the resources
+  }
+  return result;
 }
 
